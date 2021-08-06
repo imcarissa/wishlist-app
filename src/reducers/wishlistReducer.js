a8c7144ededbc34c06dbc53a75df782f0667e3f2
imcarissa <carissa.sarreal@gmail.com>
2021-08-06 06:02:41 -0400

    ADD_LIST case statement added

--- src/reducers/wishlistReducer.js
+++ src/reducers/wishlistReducer.js
@@ -2,6 +2,8 @@ export const wishlistReducer = (state = [], action) => {
     switch(action.type) {
         case 'FETCH_LISTS':
             return action.payload
+        case 'ADD_LIST':
+            return [...state, action.payload]
         default:
             return state
     }
