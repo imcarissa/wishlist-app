a684cf0934c357c4ed35348516b3f8f7d252b38f
imcarissa <carissa.sarreal@gmail.com>
2021-08-06 06:02:10 -0400

    endpoint const created. addList POST request action created

--- src/actions/wishlistActions.js
+++ src/actions/wishlistActions.js
@@ -1,7 +1,21 @@
+const endpoint = 'http://localhost:3000/api/v1/wishlists'
+
 export const fetchLists = () => {
     return (dispatch) => {
-        fetch('http://localhost:3000/api/v1/wishlists')
+        fetch(endpoint)
         .then(response => response.json())
         .then(lists => dispatch({ type: 'FETCH_LISTS', payload: lists }))
+    }
+}
+
+export const addList = list => { 
+    return (dispatch) => {
+        fetch(endpoint, {
+            method: 'POST',
+            body: JSON.stringify(list),
+            headers: { 'Content-Type': 'application/json'}
+        })
+        .then(response => response.json())
+        .then(list => dispatch({ type: 'ADD_LIST', payload: list }))
     }
 }
