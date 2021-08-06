46ef5dc886bb18566935d4eb9f423fbfc7c3b3c9
imcarissa <carissa.sarreal@gmail.com>
2021-08-06 06:48:05 -0400

    stateless components imported

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Home from '../components/Home'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import About from '../components/About'
+import WishlistContainer from '.WishlistContainer'
 
 const Router = () => {
     return (
