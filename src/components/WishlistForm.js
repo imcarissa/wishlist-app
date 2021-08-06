c82476432433c3f1b6a7754907b54cd0c2ee77fb
imcarissa <carissa.sarreal@gmail.com>
2021-08-06 04:56:57 -0400

    input fields created

---
+++ src/components/WishlistForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class WishlistForm extends React.Component {
+
+    state = {
+        name: '',
+        descripton: ''
+    }
+
+    render() {
+        return (
+            <form>
+                <h1>Create A Wishlist</h1>
+                <label>Name:</label>
+                <input type='text' value={this.state.name} onChange={this.handleChange} name='name' placeholder='list name' />
+                <br /><br />
+                <label>Description:</label>
+                <input type='text' value={this.state.description} onChange={this.handleChange} name='description' placeholder='list description' />
+                <br /><br />
+                <input type='submit' value="Create List" />
+            </form>
+        );
+    }
+}
+
+export default WishlistForm;
