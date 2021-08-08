c00602d34e0b2c5e5bbc392307aebb3eca3ee910
imcarissa <carissa.sarreal@gmail.com>
2021-08-08 03:20:37 -0400

    wishlists no longer rendered. moved to WishlistContainer

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,12 @@ import Header from './Header'
 import Footer from './Footer'
 import Router from './Router'
 import WishlistContainer from './WishlistContainer'
-import Wishlists from './Wishlists'
 
 const App = () => {
   return (
     <div className="container">
       <Header />
       <WishlistContainer />
-      <Wishlists />
       <Router />
       <Footer />
     </div>
