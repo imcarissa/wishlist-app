6789168bc4cdd48db066981b242560bd25691091
imcarissa <carissa.sarreal@gmail.com>
2021-09-04 02:02:17 -0400

    search bar change of code

--- src/components/WishlistContainer.js
+++ src/components/WishlistContainer.js
@@ -7,16 +7,7 @@ import SearchBar from '../components/SearchBar'
 
 class WishlistContainer extends React.Component {
 
-    state = { 
-        filterText: ''
-    }
-
-    handleFilter = (filterText) => {
-        this.setState({
-            filterText: filterText
-        });
-    }
-
+    
     componentDidMount() {
         this.props.fetchLists()
     };
@@ -25,12 +16,6 @@ class WishlistContainer extends React.Component {
         return (
             <div>
                 <WishlistForm /><br/>
-        
-                <SearchBar
-                    filterText={this.state.filterText}
-                    onFilterTextChange={this.handleFilter}
-                /><br/>
-
                 <Wishlists 
                     filterText={this.state.filterText}
                 />
--- src/components/Wishlists.js
+++ src/components/Wishlists.js
@@ -1,15 +1,12 @@
 import { connect } from 'react-redux'
 
 
-const Wishlists = ({ wishlist, filterText }) => {
-    
-    const searchedList = wishlist.filter( w => w.name.slice(0, filterText.length) === filterText)
-    const lists = searchedList.length === 0 ? wishlist : searchedList
+const Wishlists = ({ wishlist }) => {
 
     return (
         <div className="index">         
             <h3>Current Wishlists:</h3>
-            {lists.map(wishlist => 
+            {wishlist.map(wishlist => 
                     <ul key={wishlist.id}>
                         <li>{wishlist.name}<br/>
                          "{wishlist.description}"<br/>                   
