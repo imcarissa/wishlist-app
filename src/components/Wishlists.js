0bad7a0639dd76e0d0a2f7c6818d02429e1df40d
imcarissa <carissa.sarreal@gmail.com>
2022-01-12 01:45:55 -0500

    search bar feature added

--- src/components/Wishlists.js
+++ src/components/Wishlists.js
@@ -1,12 +1,15 @@
 import { connect } from 'react-redux'
 
 
-const Wishlists = ({ wishlist }) => {
+const Wishlists = ({ wishlist, filterText }) => {
+    
+    const searchedList = wishlist.filter( w => w.name.slice(0, filterText.length) === filterText)
+    const lists = searchedList.length === 0 ? wishlist : searchedList
 
     return (
         <div className="index">         
             <h3>Current Wishlists:</h3>
-            {wishlist.map(wishlist => 
+            {lists.map(wishlist => 
                     <ul key={wishlist.id}>
                         <li>{wishlist.name}<br/>
                          "{wishlist.description}"<br/>                   
