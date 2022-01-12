3b087ed27b8744f6458c4f6c880e906854e34dde
imcarissa <carissa.sarreal@gmail.com>
2022-01-12 01:47:40 -0500

    Credits component and Router link to Credits removed

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,11 @@
 import { Switch, Route } from 'react-router-dom'
 import About from '../components/About'
-import Credits from '../components/Credits';
 import Contact from '../components/Contact'
 
 const Router = () => {
     return (
         <Switch>
-            <Route exact path='/credits' component={Credits} />
-            <Route  path='/about' component={About} />
+            <Route exact path='/credits' component={About} />
             <Route  path='/contact' component={Contact}/>
         </Switch>
     )
